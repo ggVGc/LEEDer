1d0743802d373a9eb92883244e3fbbc9fb42795c
Sundström Valter <mail@valter.nu>
2024-07-25 22:21:03 +0200

    Add motors_test.rs and pyo3

--- src/bin/motor_test.rs
+++ src/bin/motor_test.rs
@@ -1,4 +1,3 @@
-/*
 use std::thread;
 use std::time::Duration;
 
@@ -6,9 +5,9 @@ use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
 fn main() -> PyResult<()> {
-    let arg1 = "arg1";
-    let arg2 = "arg2";
-    let arg3 = "arg3";
+    // let arg1 = "arg1";
+    // let arg2 = "arg2";
+    // let arg3 = "arg3";
     pyo3::prepare_freethreaded_python();
 
     let code = std::fs::read_to_string("src/motors.py")?;
@@ -40,9 +39,4 @@ fn main() -> PyResult<()> {
         // fun.call1(py, args)?;
         Ok(())
     })
-}
-*/
-
-fn main() {
-    
 }
